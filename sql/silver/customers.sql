5ee9e5db8829439519093ccef6331858df596e2a
yannvinnycius <yannzin1@outlook.com>
2026-05-13 11:35:53 -0300

    Correção nome da coluna customer_city na tabela customers

--- sql/silver/customers.sql
+++ sql/silver/customers.sql
@@ -5,7 +5,7 @@ SELECT
 	customer_id
 	,customer_unique_id
 	,CAST(customer_zip_code_prefix as VARCHAR) as customer_zip_code_prefix
-	,INITCAP(TRIM(customer_city))
+	,INITCAP(TRIM(customer_city)) as customer_city
 	,UPPER(customer_state) as customer_state
 FROM staging.customers;
 
