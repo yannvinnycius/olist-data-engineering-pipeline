b5272f00ec6afe5efa1e48a741dd630cf0cbc663
yannvinnycius <yannzin1@outlook.com>
2026-05-28 11:27:48 -0300

    Acrescentado função para adicionar logs de execução no pipeline

--- etl/pipeline.py
+++ etl/pipeline.py
@@ -1,17 +1,52 @@
 import subprocess
+import logging
 
+# Configuração log
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filename="pipeline.log",
+    filemode="a"
+)
+
+# Função execução
+def run_step(step_name, command):
+
+    try:
+
+        print(f"Executando: {step_name}")
+        logging.info(f"Executando: {step_name}")
+
+        subprocess.run(command, check=True)
+
+        print(f"{step_name} executado com sucesso!")
+        logging.info(f"{step_name} executado com sucesso!")
+
+    except subprocess.CalledProcessError as e:
+
+        print(f"Erro ao executar {step_name}")
+        logging.error(f"Erro ao executar {step_name}: {e}")
+
+        raise
+
+# Pipeline
 print("Iniciando pipeline...")
+logging.info("Iniciando pipeline...")
 
-#Executa staging, se erro o processo para.
-print("Executando staging...")
-subprocess.run(['python', "etl/load_staging.py"], check=True)
+run_step(
+    "Staging",
+    ["python", "etl/load_staging.py"]
+)
 
-#Executa silver, se erro o processo para.
-print("Executando silver...")
-subprocess.run(['python', "etl/run_silver.py"], check=True)
+run_step(
+    "Silver",
+    ["python", "etl/run_silver.py"]
+)
 
-#Executa gold, se erro o processo para.
-print("Executando gold...")
-subprocess.run(['python', "etl/run_gold.py"], check=True)
+run_step(
+    "Gold",
+    ["python", "etl/run_gold.py"]
+)
 
 print("Pipeline executado com sucesso!")
+logging.info("Pipeline executado com sucesso!")
